d566b6d5f58d7087b347369a6a0dcdf8f2792f5e
Benis Tambe <will.jenifer@yahoo.com>
2022-01-18 10:12:47 -0500

    commit tree 6e0ac308fd283ec6dec6412c4371042d57e67c17

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import GraceHopperQuoteComponent from "./GraceHopperQuoteComponent";
+import CatComponent from "./CatComponent";
+import MouseComponent from "./MouseComponent";
 
 class App extends Component {
 	render() {
@@ -8,8 +11,9 @@ class App extends Component {
 				<CatComponent />
 				<GraceHopperQuoteComponent />
 				{/* one more component missing */}
+				<MouseComponent />
 			</div>
 		);
 	}
-
+}
 export default App;
